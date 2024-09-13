0147d6eb899156d997acb26df592053dda97da2f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-13 22:43:09 +0300

    Added display_board method in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Board
+  def initialize(board)
+    @board = board
+  end
+
+  def display_board
+    puts "\nTIC TAC TOE \n \n"
+    @board.each do |row|
+      puts row.join(' ')
+      puts '---|---|---'
+    end
+    puts "\nTIC TAC TOE \n \n"
+  end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
 require_relative 'lib/board'
 require_relative 'lib/playeer'
 require_relative 'lib/game'
+
+b = Board.new([[' 1', '| 2', '| 3'], [' 4', '| 5', '| 6'], [' 7', '| 8', '| 9']])
+b.display_board
