74755dd53f1db806c50cb691c0644e5b2d8ffad9
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-16 11:38:06 +0300

    Update make_move method, to handle invalid player inputs

--- lib/playeer.rb
+++ lib/playeer.rb
@@ -11,6 +11,12 @@ class Player
   def make_move
     puts "#{name}, please choose where you want to place your marker (#{marker})"
     puts 'Please enter a number from 1 to 9 to identify your option:'
-    gets.chomp
+    loop do
+      input = gets.chomp
+
+      return input if input.match?(/^[1-9]$/)
+
+      puts 'Invalid input. Please enter a number between 1 and 9.'
+    end
   end
 end
