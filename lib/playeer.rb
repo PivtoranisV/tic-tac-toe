c0c650989d35ac8f45a8ce85f1b3fd532a72afd8
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-16 10:58:10 +0300

    Added Player class with make_move method

--- lib/playeer.rb
+++ lib/playeer.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Player
+  attr_reader :name, :marker
+
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
+  end
+
+  def make_move
+    puts "#{name}, please choose where you want to place your marker (#{marker})"
+    puts 'Please enter a number from 1 to 9 to identify your option:'
+    gets.chomp
+  end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,22 @@ require_relative 'lib/board'
 require_relative 'lib/playeer'
 require_relative 'lib/game'
 
+puts 'Hi everyone, welcome to the TIC TAC TOE game'
+puts 'Player 1, please enter your name'
+player1_name = gets.chomp
+puts "Hi #{player1_name}, your marker will be - X"
+puts 'Player 2, please enter your name'
+player2_name = gets.chomp
+puts "Hi #{player2_name}, your marker will be - 0"
+
 b = Board.new([[' 1', '| 2', '| 3'], [' 4', '| 5', '| 6'], [' 7', '| 8', '| 9']])
 b.display_board
+
+player1 = Player.new(player1_name, 'X')
+player2 = Player.new(player2_name, '0')
+
+p player1.make_move
+
 b.update_board('X', '1')
 b.display_board
 b.update_board('0', '2')
